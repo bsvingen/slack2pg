d282784549dfd1313672e0a9f9f05ce1d0db014d
Børge Svingen <bsvingen@borkdal.com>
2016-08-14 19:27:22 -0400

    Using spec for command line parsing

--- project.clj
+++ project.clj
@@ -3,22 +3,22 @@
   :url "https://github.com/bsvingen/slack2pg"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
                  [org.clojure/java.jdbc "0.3.7"]
-                 [amazonica "0.3.29"]
+                 [amazonica "0.3.72"]
                  [com.comoyo/condensation "0.2.2"]
                  [org.tobereplaced/lettercase "1.0.0"]
                  [com.borkdal/clojure.utils "0.1.2"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [clj-time "0.10.0"]
-                 [com.borkdal/squirrel "0.3.1"]
-                 [com.taoensso/timbre "4.0.2"]]
+                 [org.clojure/core.async "0.2.385"]
+                 [clj-time "0.12.0"]
+                 [com.borkdal/squirrel "0.3.2"]
+                 [com.taoensso/timbre "4.7.0"]]
   :main com.borkdal.slack2pg
   :target-path "target/%s"
   :plugins [[lein-bin "0.3.5"]]
   :bin {:name "slack2pg"
         :bin-path "target"}
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[midje "1.7.0"]]
-                   :plugins [[lein-midje "3.1.3"]]}})
+             :dev {:dependencies [[midje "1.9.0-alpha4"]]
+                   :plugins [[lein-midje "3.2"]]}})
--- src/com/borkdal/slack2pg.clj
+++ src/com/borkdal/slack2pg.clj
@@ -3,9 +3,19 @@
             [com.borkdal.slack2pg.sqs :as sqs]
             [com.borkdal.slack2pg.export :as export]
             [com.borkdal.slack2pg.cloudformation :as cloudformation]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [clojure.spec :as s])
   (:gen-class))
 
+(defn is?
+  [s]
+  (partial = s))
+
+(s/def ::args (s/cat :config-edn-filename string?
+                     :commands (s/alt :sqs (is? "sqs")
+                                      :aws-create (is? "aws-create")
+                                      :read-export (s/cat :command (is? "read-export")
+                                                          :slack-export-pathname string?))))
 (defn- error
   []
   (println "See README.md for instructions on how to run.")
@@ -13,22 +23,22 @@
 
 (defn -main
   ([& args]
-   (let [[config-edn-filename
-          command
-          slack-export-pathname] args]
-     (when-not (and config-edn-filename
-                    command)
+   (let [parsed-args (s/conform ::args args)]
+     (when (= parsed-args :clojure.spec/invalid)
        (error))
-     (config/with-config-file [config-edn-filename]
-       (case command
-         "sqs" (loop []
-                 (try
-                   (sqs/write-all-sqs-messages-to-database)
-                   (catch Throwable e
-                     (println "Caught" e)
-                     (println "Retrying")))
-                 (recur))
-         "aws-create" (cloudformation/deploy)
-         "read-export" (export/write-export-messages-to-database slack-export-pathname)
-         (error))))))
+     (let [{:keys [config-edn-filename commands]} parsed-args
+           [command options] commands]
+       (config/with-config-file [config-edn-filename]
+         (case command
+           :sqs (loop []
+                  (try
+                    (sqs/write-all-sqs-messages-to-database)
+                    (catch Throwable e
+                      (println "Caught" e)
+                      (println "Retrying")))
+                  (recur))
+           :aws-create (cloudformation/deploy)
+           :read-export (export/write-export-messages-to-database
+                         (:slack-export-pathname options))
+           (error)))))))
 
